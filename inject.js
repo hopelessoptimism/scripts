9e589527c3f45f0b223588599cfc795548514bce
Jonathan Dinu <jondinu@gmail.com>
2015-10-24 16:06:09 -0700

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,4 +1,8 @@
-var load_library = function(url) {
+var load_library = function(lib, version) {
+  // use cdnjs
+  var base = "https://cdnjs.cloudflare.com/ajax/libs/"
+  var url = base + lib + '/' + version + '/' + lib + '.min.js'
+  
   var script = document.createElement("script");
   script.src = url;
   document.body.appendChild(script);
